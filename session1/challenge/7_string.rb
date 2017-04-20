016a438093eecb59f34d3bf2ab6cbd70f79daa99
Panteha <panteha.ahmadi@gmail.com>
2017-04-20 13:06:29 +0100

    Returns the character after every letter r

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,12 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+	new_arr = string.downcase.split("")
+	sec_arr = []
+	for i in 0..new_arr.count
+		if new_arr[i] == "r" 
+			sec_arr.push(new_arr[i+1])
+		end
+	end
+	sec_arr.join
 end
