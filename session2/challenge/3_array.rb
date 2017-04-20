480da0b9978289e76b85c5a9b59d05beb1b558da
Panteha <panteha.ahmadi@gmail.com>
2017-04-20 18:41:42 +0100

    Returns a string containing every other character of a string

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,12 @@
 
 class String
   def every_other_char
+    return "" if self == ""
+    new_string = ""
+    self.size.times do |index|
+      next if index.odd?
+      new_string << self[index]
+    end
+    return new_string
   end
 end
